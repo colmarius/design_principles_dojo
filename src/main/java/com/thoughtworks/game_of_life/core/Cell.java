13c1a8498a708718919b39d341ef409332aafd63
Marius Colacioiu <marius.colacioiu@gmail.com>
2014-07-23 00:25:49 +0200

    Update cell interface.
    Add mutate method. Drop willBeAlive method.

--- src/main/java/com/thoughtworks/game_of_life/core/Cell.java
+++ src/main/java/com/thoughtworks/game_of_life/core/Cell.java
@@ -2,5 +2,5 @@ package com.thoughtworks.game_of_life.core;
 
 public interface Cell {
     boolean isAlive();
-    boolean willBeAlive(int numberOfAliveNeighbours);
+    Cell mutate(int numberOfAliveNeighbours);
 }
